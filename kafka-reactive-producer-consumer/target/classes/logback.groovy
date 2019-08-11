23281723fb23ee53964832e12d735bb0aa5eaa7b
perkss <stuart.m.perks@gmail.com>
2019-08-11 10:46:41 +0100

    Revert "Revert "Add initial module for producer and consumer.""
    This reverts commit 0298bb35

--- kafka-reactive-producer-consumer/target/classes/logback.groovy
+++
@@ -1,8 +0,0 @@
-import ch.qos.logback.classic.encoder.PatternLayoutEncoder
-
-appender("CONSOLE", ConsoleAppender) {
-    encoder(PatternLayoutEncoder) {
-        pattern = "%-4relative [%thread] - %msg%n"
-    }
-}
-root(INFO, ["CONSOLE"])
