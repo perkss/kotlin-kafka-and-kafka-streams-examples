96413795347072c03323cea8ce3d8359c1ada2e0
perkss <stuart.m.perks@gmail.com>
2020-04-28 21:09:27 +0100

    Streams topology test driver.

--- kotlin-kafka-streams/src/main/kotlin/com/perkss/kafka/reactive/OrderProcessingTopology.kt
+++ kotlin-kafka-streams/src/main/kotlin/com/perkss/kafka/reactive/OrderProcessingTopology.kt
@@ -9,12 +9,12 @@ import java.util.*
 
 fun stock(streamsBuilder: StreamsBuilder,
           props: AppProperties): KTable<String, String> =
-        streamsBuilder.table(props.stockInventory, Consumed.with(Serdes.String(), Serdes.String()))
+        streamsBuilder.table(props.stockInventory, Consumed.with(Serdes.String(), Serdes.String()), Materialized.`as`(props.stockInventory))
 
 fun customer(
         streamsBuilder: StreamsBuilder,
         props: AppProperties): GlobalKTable<String, String> =
-        streamsBuilder.globalTable(props.customerInformation, Consumed.with(Serdes.String(), Serdes.String()))
+        streamsBuilder.globalTable(props.customerInformation, Consumed.with(Serdes.String(), Serdes.String()), Materialized.`as`(props.customerInformation))
 
 
 fun orderProcessingTopology(streamConfig: Properties,
@@ -26,14 +26,19 @@ fun orderProcessingTopology(streamConfig: Properties,
                             valSerde: Serde<String>): Topology {
     streamsBuilder
             .stream(props.orderRequest, Consumed.with(keySerde, valSerde))
-            .leftJoin(stockTable) { leftValue: String, rightValue: String? -> leftValue + rightValue }
+            .peek { key, value -> println(key + " " + value) }
+            .leftJoin(stockTable, { leftValue: String, rightValue: String? -> leftValue + rightValue },
+                    Joined.with(keySerde, valSerde, valSerde))
+            .peek { key, value -> println(key + " " + value) }
             .leftJoin(customerTable,
                     KeyValueMapper { leftKey: String, leftValue: String -> leftKey },
                     ValueJoiner { leftValue: String, rightValue: String? -> rightValue })
+            .peek { key, value -> println(key + " " + value) }
             .mapValues { key, keyValue ->
                 keyValue
             }
-            .to(props.outputTopic)
+            .peek { key, value -> println(key + " " + value) }
+            .to(props.outputTopic, Produced.with(keySerde, valSerde))
     // pass to override for optimization
     return streamsBuilder.build(streamConfig)
 }
--- kotlin-kafka-streams/src/test/kotlin/com/perkss/kafka/reactive/OrderProcessingTopologyTest.kt
+++ kotlin-kafka-streams/src/test/kotlin/com/perkss/kafka/reactive/OrderProcessingTopologyTest.kt
@@ -1,8 +1,13 @@
 package com.perkss.kafka.reactive
 
+import io.confluent.common.utils.TestUtils
 import io.confluent.kafka.serializers.KafkaAvroSerializerConfig
 import io.confluent.kafka.streams.serdes.avro.PrimitiveAvroSerde
-import org.apache.kafka.streams.*
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.streams.KeyValue
+import org.apache.kafka.streams.StreamsBuilder
+import org.apache.kafka.streams.StreamsConfig
+import org.apache.kafka.streams.TopologyTestDriver
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
@@ -14,6 +19,7 @@ internal class OrderProcessingTopologyTest {
     private val schemaRegistryScope: String = OrderProcessingTopologyTest::class.java.getName()
     private val mockSchemaRegistryUrl = "mock://$schemaRegistryScope"
 
+    // seerdes messed up with avro
     @Test
     fun orderProcessing() {
         val builder = StreamsBuilder()
@@ -22,27 +28,28 @@ internal class OrderProcessingTopologyTest {
         val appId = "testOrderProcessing"
         val broker = "dummy:1234"
 
-        props[StreamsConfig.APPLICATION_ID_CONFIG] = appId
-        props[StreamsConfig.BOOTSTRAP_SERVERS_CONFIG] = broker
+        props[StreamsConfig.APPLICATION_ID_CONFIG] = "test"
+        props[StreamsConfig.BOOTSTRAP_SERVERS_CONFIG] = "dummy:1234"
         props[StreamsConfig.TOPOLOGY_OPTIMIZATION] = StreamsConfig.OPTIMIZE
         // TODO something is using these ouch
         props[StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG] = PrimitiveAvroSerde::class.java
         props[StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG] = PrimitiveAvroSerde::class.java
         props[KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG] = mockSchemaRegistryUrl
+        props[StreamsConfig.STATE_DIR_CONFIG] = TestUtils.tempDirectory().path
 
         val config = mapOf(
                 KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG to mockSchemaRegistryUrl)
 
-        val keySerde = PrimitiveAvroSerde<String>()
+        val keySerde = Serdes.String()
         val valSerde = PrimitiveAvroSerde<String>()
 
         // configure with schema registry
-        keySerde.configure(config, true)
+        // keySerde.configure(config, true)
         valSerde.configure(config, false)
 
         val appProperties = AppProperties()
-        appProperties.applicationId = appId
-        appProperties.bootstrapServers = broker
+        appProperties.applicationId = "test"
+        appProperties.bootstrapServers = "dummy:1234"
         appProperties.customerInformation = "customer"
         appProperties.stockInventory = "stock"
         appProperties.orderRequest = "order-request"
@@ -56,17 +63,23 @@ internal class OrderProcessingTopologyTest {
 
         val testDriver = TopologyTestDriver(topology, props)
 
+        val key = "1234A"
+
         val stockTopic = testDriver.createInputTopic(appProperties.stockInventory, keySerde.serializer(), valSerde.serializer())
-        stockTopic.pipeInput("key", "stock-matched")
+        stockTopic.pipeInput(key, "stock-matched")
+
+        //      val tableOutput = testDriver.createOutputTopic(appProperties.stockInventory, keySerde.deserializer(), valSerde.deserializer())
+//        assertThat(tableOutput.readKeyValue(), equalTo(KeyValue(key, "null")))
 
         val customerTopic = testDriver.createInputTopic(appProperties.customerInformation, keySerde.serializer(), valSerde.serializer())
-        customerTopic.pipeInput("key", "customer-matched")
+        customerTopic.pipeInput(key, "customer-matched")
 
         val inputTopic = testDriver.createInputTopic(appProperties.orderRequest, keySerde.serializer(), valSerde.serializer())
-        inputTopic.pipeInput("key", "value")
+        inputTopic.pipeInput(key, "value")
 
-        val outputTopic: TestOutputTopic<String, String> = testDriver.createOutputTopic(appProperties.outputTopic, keySerde.deserializer(), valSerde.deserializer())
-        assertThat(outputTopic.readKeyValue(), equalTo(KeyValue("key", "customer-matched")))
+        val outputTopic = testDriver.createOutputTopic(appProperties.outputTopic, keySerde.deserializer(), valSerde.deserializer())
+        assertThat(outputTopic.readKeyValue(), equalTo(KeyValue(key, "customer-matched")))
 
+        testDriver.close()
     }
 }
