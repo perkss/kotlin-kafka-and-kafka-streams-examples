5d3e8b8570bc5a6078584c79c1567f57d6da5036
Stuart Perks <stuart.m.perks@gmail.com>
2020-05-16 10:51:31 +0100

    Timestamp extractor example. (#8)

--- kotlin-kafka-streams-examples/src/main/kotlin/com/perkss/kafka/reactive/examples/AggregateExamples.kt
+++ kotlin-kafka-streams-examples/src/main/kotlin/com/perkss/kafka/reactive/examples/AggregateExamples.kt
@@ -2,14 +2,20 @@ package com.perkss.kafka.reactive.examples
 
 import com.perkss.social.media.model.PostCreated
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.Topology
-import org.apache.kafka.streams.kstream.Consumed
-import org.apache.kafka.streams.kstream.KTable
-import org.apache.kafka.streams.kstream.Produced
+import org.apache.kafka.streams.kstream.*
+import org.apache.kafka.streams.processor.TimestampExtractor
+import org.slf4j.LoggerFactory
+import java.time.Duration
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 object AggregateExamples {
+
+    private val logger = LoggerFactory.getLogger(AggregateExamples::class.java)
 
     fun buildUserSocialMediaPostsTotalCountTopology(inputTopic: String,
                                                     outputTopic: String,
@@ -25,6 +31,38 @@ object AggregateExamples {
                 .count()
         // stream the total counts per user keyed by user ID and the count
         aggregated.toStream().to(outputTopic, Produced.with(Serdes.String(), Serdes.Long()))
+        return builder.build()
+    }
+
+    fun buildUserSocialMediaPostsWindowedTotalCountTopology(inputTopic: String,
+                                                            outputTopic: String,
+                                                            postCreatedSerde: SpecificAvroSerde<PostCreated>): Topology {
+        // custom timestamp extractor
+        val timestampExtractor = TimestampExtractor { record: ConsumerRecord<Any, Any>, previousTimestamp: Long ->
+            if (record.value() is PostCreated) {
+                LocalDateTime.parse((record.value() as PostCreated).timestamp)
+                        .toInstant(ZoneOffset.UTC).toEpochMilli()
+            } else previousTimestamp
+        }
+
+        val builder = StreamsBuilder()
+
+        // consume the post created
+        val input = builder.stream(inputTopic, Consumed.with(Serdes.String(), postCreatedSerde,
+                timestampExtractor, Topology.AutoOffsetReset.EARLIEST))
+
+        // Build a table of the counts
+        val aggregated: KTable<Windowed<String>, Long> = input
+                .peek { key, postCreated -> logger.info("Key {} and value {}", key, postCreated) }
+                .groupBy { _, value -> value.userId } // group by the user who created the post
+                .windowedBy(
+                        TimeWindows.of(Duration.ofSeconds(30)))
+                .count()
+
+        // stream the total counts per user keyed by user ID and the count
+        aggregated.toStream { windowedKey, _ -> windowedKey.key() }
+                .peek { key, value -> logger.info("Sending on Key {} value {}", key, value) }
+                .to(outputTopic, Produced.with(Serdes.String(), Serdes.Long()))
         return builder.build()
     }
 
--- kotlin-kafka-streams-examples/src/test/kotlin/com/perkss/kafka/reactive/examples/AggregateExamplesTest.kt
+++ kotlin-kafka-streams-examples/src/test/kotlin/com/perkss/kafka/reactive/examples/AggregateExamplesTest.kt
@@ -10,7 +10,9 @@ import org.apache.kafka.streams.KeyValue
 import org.apache.kafka.streams.TopologyTestDriver
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
@@ -20,21 +22,24 @@ internal class AggregateExamplesTest {
 
     private val schemaRegistryScope: String = AggregateExamplesTest::class.java.name
     private val mockSchemaRegistryUrl = "mock://$schemaRegistryScope"
+    private val inputTopicName = "post-created"
+    private val outputTopicName = "user-posts-total-count"
+
+    private val config = mapOf(
+            KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG to mockSchemaRegistryUrl)
+
+    private val props = TestProperties.properties("aggregate-example-app", "test-host:9092")
+
+    private val postCreatedSerde = SpecificAvroSerde<PostCreated>()
+
+    @BeforeEach
+    fun setup() {
+        postCreatedSerde.configure(config, false)
+        props[KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG] = mockSchemaRegistryUrl
+    }
 
     @Test
     fun buildUserSocialMediaPostsCountTopology() {
-        val props = TestProperties.properties("aggregate-example-app", "test-host:9092")
-        props[KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG] = mockSchemaRegistryUrl
-
-        val inputTopicName = "post-created"
-        val outputTopicName = "user-posts-total-count"
-
-        val config = mapOf(
-                KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG to mockSchemaRegistryUrl)
-
-        val postCreatedSerde = SpecificAvroSerde<PostCreated>()
-        postCreatedSerde.configure(config, false)
-
         val totalUserSocialMediaPostsTopology = AggregateExamples.buildUserSocialMediaPostsTotalCountTopology(inputTopicName, outputTopicName, postCreatedSerde)
         val testDriver = TopologyTestDriver(totalUserSocialMediaPostsTopology, props)
 
@@ -66,4 +71,91 @@ internal class AggregateExamplesTest {
         testDriver.close()
         MockSchemaRegistry.dropScope(schemaRegistryScope)
     }
+
+    @Test
+    fun buildWindowedUserSocialMediaPostsCountTopology() {
+        val props = TestProperties.properties("aggregate-example-app", "test-host:9092")
+        props[KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG] = mockSchemaRegistryUrl
+
+        val inputTopicName = "post-created"
+        val outputTopicName = "user-posts-total-count"
+
+        val config = mapOf(
+                KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG to mockSchemaRegistryUrl)
+
+        val postCreatedSerde = SpecificAvroSerde<PostCreated>()
+        postCreatedSerde.configure(config, false)
+
+        val totalUserSocialMediaPostsTopology = AggregateExamples.buildUserSocialMediaPostsWindowedTotalCountTopology(inputTopicName, outputTopicName, postCreatedSerde)
+        val testDriver = TopologyTestDriver(totalUserSocialMediaPostsTopology, props)
+
+        val postCreatedTopic = testDriver.createInputTopic(inputTopicName,
+                Serdes.String().serializer(), postCreatedSerde.serializer())
+
+        val alice = UUID.randomUUID().toString()
+        val bill = UUID.randomUUID().toString()
+        val jasmine = UUID.randomUUID().toString()
+
+        val eventTimeStamp1 = Instant.now()
+        // #Window 1
+        postCreatedTopic.pipeInput(UUID.randomUUID().toString(),
+                PostCreated(UUID.randomUUID().toString(),
+                        alice,
+                        "Happy",
+                        eventTimeStamp1.formatInstantToIsoDateTime()))
+
+        // Move 20 seconds forward in time #Window 1
+        val eventTimeStamp2 = eventTimeStamp1.plusSeconds(20)
+
+        postCreatedTopic.pipeInput(UUID.randomUUID().toString(),
+                PostCreated(
+                        UUID.randomUUID().toString(),
+                        bill,
+                        "Party",
+                        eventTimeStamp2.formatInstantToIsoDateTime()))
+
+        // Move 20 seconds forward in time #Window 1
+        val eventTimeStamp3 = eventTimeStamp2.plusSeconds(20)
+
+        postCreatedTopic.pipeInput(UUID.randomUUID().toString(),
+                PostCreated(
+                        UUID.randomUUID().toString(),
+                        alice,
+                        "Running",
+                        eventTimeStamp3.formatInstantToIsoDateTime()))
+
+        // New window
+        // Move 2 seconds forward in time #Window 2
+        val eventTimeStamp4 = eventTimeStamp3.plusSeconds(2)
+
+        postCreatedTopic.pipeInput(UUID.randomUUID().toString(),
+                PostCreated(UUID.randomUUID().toString(), jasmine, "Drinking",
+                        eventTimeStamp4.formatInstantToIsoDateTime()))
+
+        // Move 1 seconds forward in time #Window 2
+        val eventTimeStamp5 = eventTimeStamp4.plusSeconds(1)
+
+        postCreatedTopic.pipeInput(UUID.randomUUID().toString(),
+                PostCreated(UUID.randomUUID().toString(), alice, "Travelling",
+                        eventTimeStamp5.formatInstantToIsoDateTime()))
+
+        val outputTopic = testDriver.createOutputTopic(outputTopicName, Serdes.String().deserializer(),
+                Serdes.Long().deserializer())
+
+        // Alice has a single post
+        assertThat(outputTopic.readKeyValue(), equalTo(KeyValue(alice, 1L)))
+        assertThat(outputTopic.readKeyValue(), equalTo(KeyValue(bill, 1L)))
+        // Alice has a second post
+        assertThat(outputTopic.readKeyValue(), equalTo(KeyValue(alice, 1L)))
+        assertThat(outputTopic.readKeyValue(), equalTo(KeyValue(jasmine, 1L)))
+        // Alice has a third post which is in the same window as her second post so aggregate
+        assertThat(outputTopic.readKeyValue(), equalTo(KeyValue(alice, 2L)))
+
+        testDriver.close()
+        MockSchemaRegistry.dropScope(schemaRegistryScope)
+    }
+
+    private fun Instant.formatInstantToIsoDateTime(): String =
+            LocalDateTime.ofInstant(this, ZoneOffset.UTC).format(DateTimeFormatter.ISO_DATE_TIME)
+
 }
